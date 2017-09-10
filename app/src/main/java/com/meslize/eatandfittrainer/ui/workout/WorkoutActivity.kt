5c890d9f9d2adcecff96f1c0615fa598ff685ab0
jamesbluecrow <jamesbluecrow@gmail.com>
2017-09-10 20:21:13 +0100

    Added CongratulationsActivity

--- app/src/main/java/com/meslize/eatandfittrainer/ui/workout/WorkoutActivity.kt
+++ app/src/main/java/com/meslize/eatandfittrainer/ui/workout/WorkoutActivity.kt
@@ -1,13 +1,18 @@
 package com.meslize.eatandfittrainer.ui.workout
 
-import android.support.v7.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import com.meslize.eatandfittrainer.R
+import com.meslize.eatandfittrainer.ui.congratulations.CongratulationsActivity
+import kotlinx.android.synthetic.main.activity_workout.*
 
 class WorkoutActivity : AppCompatActivity() {
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_workout)
+
+    stop.setOnClickListener { startActivity(Intent(this, CongratulationsActivity::class.java)) }
   }
 }
